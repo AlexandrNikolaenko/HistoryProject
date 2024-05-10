6fc896e2967ed9cf74a29a8c7a8491b15de57ad4
Александр <nikol.alex06@mail.ru>
2024-05-11 00:21:15 +0300

    Продолжаю работу над страницей хронолгии

--- js/history.js
+++ js/history.js
@@ -5,24 +5,31 @@ var condition = {openElem: null};
 const years = ['4554', '5564', '6574', '7584', '8591'];
 
 class ChronoElement{
-    constructor(text, url, id){
+    constructor(text, url, id, arrow){
         this.text = text;
         this.imgUrl = url;
-        this.number = id
+        this.number = id;
+        if (arrow.startPoint) this.startPoint = arrow.startPoint;
+        if (arrow.endPoint) this.endPoint = arrow.startPoint;
     }
 
     createOpenFact(){
+        let close = elt('div', {class: 'close'}, elt('img', {src: '../img/close.svg'}));
         let element = elt('div', {class: 'open-fact'},
-            elt('div', {class: 'full-img-block'}, elt('img', )),
+            elt('div', {class: 'full-img-block'}, elt('img', {src: this.imgUrl})),
             elt('div', {class:'full-text-block'}, this.text),
-            elt('div', {class: 'close'}, elt('img', {src: '../img/close.svg'}))
+            close
         );
         condition.openElem = element;
+        close.addEventListener('click', () => {
+            element.remove(); 
+            condition.openElem = null
+        });
         return element;
     }
 
     eventElem(){
-
+        
     }
 
     createElem(){
@@ -30,12 +37,14 @@ class ChronoElement{
             elt('div', {class: 'fact-img-block'}, elt('img', {src: this.imgUrl})),
             elt('div', {class: 'fact-short-text'}, text)
         )
+        element.addEventListener('click', () => this.eventElem());
+        return element;
     }
 }
 
 class Chronolent{
     constructor(startPoint){
-        this.startPoint = [];
+        this.startPoint = startPoint;
     }
 
     lentFunc(x){
